c4d773c5a7bb6e8583e7fe6f7a1f143f143ac1ed
hetu9898 <hetu22849@gmail.com>
2024-03-06 17:35:09 +0530

    final code
    DiGi store aplication

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import Colors  from "colors";
+const connectDB = async () => {
+    try {
+     const conn = await mongoose.connect(process.env.MONGO_URL);
+     console.log(`Connection To Mongodb Database ${conn.connection.host}`.bgMagenta.white
+     );
+     } catch (error) {
+       console.log(`Errro in Mangodb ${error}`.bgRed.white);
+     }
+};
+ export default connectDB;
