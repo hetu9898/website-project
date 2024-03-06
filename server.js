c05909d5bb6b34501526052ac85e3c03eb1fbe45
hetu9898 <158603062+hetu9898@users.noreply.github.com>
2024-03-06 18:55:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoute.js';
 import categoryRouts from "./routes/categoryRoutes.js"
 import productRoutes from'./routes/productRoutes.js'
 import cors from 'cors';
-
+import path from 'path';
 import {fileURLToPath} from 'url';
 
 //config env
